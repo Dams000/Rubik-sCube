aa7ad5dd01a17e518f977579cd05e2b7f79e6f8c
barrage <damienboyer10@gmail.com>
2024-05-01 18:33:50 +0200

    Changing build script.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc -Wall -Wextra -o rubiks cublet.c cube.c rubiksCube.c -L ../raylib/src -lraylib -lm
+gcc -Wall -Wextra -o rubiks cublet.c cube.c scramble.c rubiksCube.c -L ../raylib/src -lraylib -lm
